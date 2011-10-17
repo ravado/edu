9a37e0974b83cef6b3558401de9dbd52598a0bf9
includ <theinclud@yandex.ru>
2011-10-17 23:05:11 +0300

    [admin0017]

---
+++ application/classes/controller/adm/news.php
@@ -0,0 +1,28 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/*
+ * Контроллер отвечает за главную странцу админки
+ * 
+ */
+class Controller_Adm_News extends Controller_Base{
+
+    public $template = "vBase";
+    public $data = array();
+
+   public function action_index(){
+
+   }
+
+   /*Показываем страницу добавления новости*/
+   public function action_add(){
+       $this->template->title = "Добавить новость";
+       $this->template->styles = array("stfile/css/adm.css" => "screen");
+       $this->template->scripts = array('stfile/js/adm.js');
+
+       $data['page'] = View::factory('adm/vAdmNewsAdd');
+       $data['pageFlag'] = ': Добавление новости';
+       
+       $this->template->content = View::factory('adm/vAdm',$data);
+   }
+
+
+}
