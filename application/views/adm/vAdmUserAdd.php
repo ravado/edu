b775f5263c12bc4e1904a8c21f69ba5931fb1adc
includ <theinclud@yandex.ru>
2011-10-18 00:12:27 +0300

    [admin0018]  [+] Silver кнопки с иконками

--- application/views/adm/vAdmUserAdd.php
+++ application/views/adm/vAdmUserAdd.php
@@ -1,10 +1,11 @@
-<input type="text" class="inpColor" placeholder="Имя пользователя">
+<input type="text" name="login" class="inpColor"  placeholder="Имя пользователя">
 <input type="text" class="inpColor" placeholder="Пароль пользователя">
 <input type="text" class="inpColor" placeholder="Адрес электронной почты">
-<span>Пол: <input type="radio" name="sex">Мужской <input type="radio" name="sex">Женский</span>
+<span id="sex">Пол: <input type="radio" name="sex">Мужской <input type="radio" name="sex">Женский</span>
 <input type="text" class="inpColor" placeholder="Настоящее имя">
 <input type="text" class="inpColor" placeholder="Настоящая фамилия">
 <select class="inpColor">
     <option>Обычный пользователь</option>
     <option>Администратор сайта</option>
 </select>
+<a class="btnSilver SaveUser">Сохранить пользователя</a>
