1a76eedfc2d04cfad2e7d5c73cd2baa087e87c6b
ravado <the.ravado@gmail.com>
2011-10-23 21:01:26 +0300

    [admin0025]  [+] На форме входа нажатие кнопки Enter равносильна нажатию кнопки "Войти"

--- stfile/js/home.js
+++ stfile/js/home.js
@@ -67,3 +67,11 @@ function checkEmail(){
         $("#spnHintResEmail").css('visibility','hidden');
     }
 }
+
+$(document).ready(function(){
+   $("body").keypress(function(e){
+       if(e.keyCode == 13){
+           auth();
+       }
+   })
+});
